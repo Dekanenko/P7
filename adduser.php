996c199a562e2d0e71db4b046a9e9d63f92e4a9b
Dekanenko <kirilldekanenko@gmail.com>
2021-11-03 19:22:38 +0300

    P8

--- adduser.php
+++ adduser.php
@@ -1,3 +1,11 @@
+<?php
+    session_start();
+    $isRestricted = false;
+    if (isset($_SESSION['auth']) && $_SESSION['auth'] === true) {
+        $isRestricted = true;
+}?>
+
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -17,45 +25,52 @@
 </head>
 <body>
 <div class="container">
-   <h3>Add New User</h3>
-   <form action="handler.php" method="post" enctype="multipart/form-data">
-       <div class="row">
-           <div class="field">
-               <label>Name: <input type="text" name="name"></label>
-           </div>
-       </div>
-       <div class="row">
-           <div class="field">
-               <label>E-mail: <input type="email" name="email"><br></label>
-           </div>
-       </div>
-       <div class="row">
-           <div class="field">
-               <label>
-                   <input class="with-gap" type="radio" name="gender" value="female"/>
-                   <span>Female</span>
-               </label>
-           </div>
-           <div class="field">
-               <label>
-                   <input class="with-gap"  type="radio" name="gender" value="male"/>
-                   <span>Male</span>
-               </label>
-           </div>
-       </div>
-       <div class="row">
-           <div class="file-field input-field">
-               <div class="btn">
-                   <span>Photo</span>
-                   <input type="file" name="photo" accept="image/png, image/gif, image/jpeg">
-               </div>
-               <div class="file-path-wrapper">
-                   <input class="file-path validate" type="text">
-               </div>
-           </div>
-       </div>
-       <input type="submit" name="submit" class="btn" value="Add">
-   </form>
+    <?php if(!$isRestricted):?>
+        <h3>Add New User</h3>
+        <form action="handler.php" method="post" enctype="multipart/form-data">
+            <div class="row">
+                <div class="field">
+                    <label>Name: <input type="text" name="name"></label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="field">
+                    <label>E-mail: <input type="email" name="email"><br></label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="field">
+                    <label>
+                        <input class="with-gap" type="radio" name="gender" value="female"/>
+                        <span>Female</span>
+                    </label>
+                </div>
+                <div class="field">
+                    <label>
+                        <input class="with-gap"  type="radio" name="gender" value="male"/>
+                        <span>Male</span>
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="file-field input-field">
+                    <div class="btn">
+                        <span>Photo</span>
+                        <input type="file" name="photo" accept="image/png, image/gif, image/jpeg">
+                    </div>
+                    <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text">
+                    </div>
+                </div>
+            </div>
+            <input type="submit" name="submit" class="btn" value="Add">
+        </form>
+        
+    <?php else:?>
+        <span>
+           You are already loged in <a href="logout.php"><input type="button" class="btn" value="Logout"></a>
+       </span>
+   <?php endif;?>
 </div>
 </body>
 </html>
